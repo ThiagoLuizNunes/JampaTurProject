a29ca14f2e84627dcfea1f981d9fba43e5389edd
Thiago Luiz <thiago.luiz_surf@hotmail.com>
2015-03-13 03:43:20 -0300

    Commit mudanção projeto JampaTur, em casa.

--- app/src/main/java/com/example/milton/jampaturproject/JampaTurActivity.java
+++ app/src/main/java/com/example/milton/jampaturproject/JampaTurActivity.java
@@ -13,7 +13,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 public class JampaTurActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
